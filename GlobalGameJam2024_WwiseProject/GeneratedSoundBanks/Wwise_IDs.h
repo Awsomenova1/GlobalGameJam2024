e27e21d3783495f8faca5b533bb1eb08427eded2
Scrubbeh <danielwangusi@gmail.com>
2024-01-31 20:52:09 +0000

    Added Stop_All event, adjusted laughter RTPC

--- GlobalGameJam2024_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
+++ GlobalGameJam2024_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -33,6 +33,7 @@ namespace AK
         static const AkUniqueID STATE_INTENSE = 2575031991U;
         static const AkUniqueID STATE_MEDIATE = 1244834824U;
         static const AkUniqueID STATE_SILENT = 2880738500U;
+        static const AkUniqueID STOP_ALL = 452547817U;
         static const AkUniqueID STOP_MENU = 2914981333U;
         static const AkUniqueID STOP_MUSIC = 2837384057U;
     } // namespace EVENTS
@@ -69,19 +70,6 @@ namespace AK
 
     namespace SWITCHES
     {
-        namespace LAUGH_METER
-        {
-            static const AkUniqueID GROUP = 2133615622U;
-
-            namespace SWITCH
-            {
-                static const AkUniqueID A_FULL = 1892317246U;
-                static const AkUniqueID B_NEAR_FULL = 4039310350U;
-                static const AkUniqueID C_HALF = 2988341216U;
-                static const AkUniqueID D_NEAR_EMPTY = 1959799710U;
-            } // namespace SWITCH
-        } // namespace LAUGH_METER
-
         namespace NPC_DIALOGUE
         {
             static const AkUniqueID GROUP = 1541636155U;
